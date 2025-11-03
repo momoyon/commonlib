08ecd7965b4cb5e78048f5a8d290deddb7873594
ictadmin <ictadm@jawakara.com>
2025-11-03 18:12:47 +0600

    [test.sh] Use absolute-ish path for RUN_CMD

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 export SRC_SUFFIX=.c
 export TESTS_DIR=./tests
 export BUILD_CMD="gcc -std=c11 -o {test_name} {test_name}.{src_suffix}"
-export RUN_CMD="./{test_name}"
+export RUN_CMD="./tests/{test_name}"
 
 python ./test.py $*
