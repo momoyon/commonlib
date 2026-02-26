786292b2de9f65ab9c237e15af04b036f2ed06af
ictadmin <ictadm@jawakara.com>
2026-02-26 17:11:55 +0600

    Remove os_list_files() test

--- tests/os_funcs.c
+++ tests/os_funcs.c
@@ -5,16 +5,16 @@
 int main(void) {
     log_info("%s", os_file_exists(__FILE__) ? "This file exists!" : "This file doesn't exist!?");
 
-    String_array files = os_list_files(".");
-
-    log_debug("%s", "os_list_file(\".\")");
-    for (int i = 0; i < files.count; ++i) {
-        log_info("FILE: %s", files.items[i]);
-
-        free(files.items[i]);
-    }
-
-    darr_free(files);
+    // String_array files = os_list_files(".");
+    //
+    // log_debug("%s", "os_list_file(\".\")");
+    // for (int i = 0; i < files.count; ++i) {
+    //     log_info("FILE: %s", files.items[i]);
+    //
+    //     free(files.items[i]);
+    // }
+    //
+    // darr_free(files);
 
     return 0;
 }
